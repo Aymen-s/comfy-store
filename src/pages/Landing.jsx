70f61739c01caaedb9f790110d5c02bcc7a4b736
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-10-28 21:58:20 +0300

    complete the featured products component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Hero } from "../components";
+import { FeaturedProducts, Hero } from "../components";
 import { customFetch } from "../utils";
 const url = "/products?featured=true";
 
@@ -12,6 +12,7 @@ function Landing() {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   );
 }
