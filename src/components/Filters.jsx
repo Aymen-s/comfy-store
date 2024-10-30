28d8ee7275f130ef390f65a02501c01b54997bea
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-10-30 09:38:38 +0300

    fix some filters component issues

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,8 @@ import FormRange from "./FormRange";
 import FormCheckbox from "./FormCheckbox";
 
 function Filters() {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -14,6 +15,7 @@ function Filters() {
         label="search product"
         name="search"
         size="input-sm"
+        defaultValue={search}
       />
       {/* CATEGORIES */}
       <FormSelect
@@ -21,6 +23,7 @@ function Filters() {
         name="category"
         list={meta.categories}
         size="select-sm"
+        defaultValue={category}
       />
       {/* ORDER */}
       <FormSelect
@@ -28,11 +31,22 @@ function Filters() {
         name="order"
         list={["a-z", "z-a", "high", "low"]}
         size="select-sm"
+        defaultValue={order}
       />
       {/* PRICE */}
-      <FormRange name="price" label="select price" size="range-sm" />
+      <FormRange
+        name="price"
+        label="select price"
+        size="range-sm"
+        price={price}
+      />
       {/* SHIPPING */}
-      <FormCheckbox label="free shiping" name="shipping" size="checkbox-sm" />
+      <FormCheckbox
+        label="free shipping"
+        name="shipping"
+        size="checkbox-sm"
+        defaultValue={shipping}
+      />
       {/* COMPANIES */}
       <FormSelect
         label="select company"
